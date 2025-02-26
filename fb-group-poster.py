b8832fec1c52165e42fde06ad3c171d32df103d7
Axioz1234 <lukedevries10@gmail.com>
2025-02-26 05:53:39 +0000

    Fix import: use 'import apify as Apify' for correct module resolution

--- fb-group-poster.py
+++ fb-group-poster.py
@@ -1,7 +1,7 @@
 import os
 import json
 import time
-import Apify # type: ignore
+import apify as Apify
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
