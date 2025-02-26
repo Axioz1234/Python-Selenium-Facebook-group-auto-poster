0d20d90c9c56d4ff49ed2cbbd57bcdd551823ea8
Axioz1234 <lukedevries10@gmail.com>
2025-02-26 05:29:30 +0000

    Bump actor version to 0.1 and finalise code

--- fb-group-poster.py
+++ fb-group-poster.py
@@ -1,7 +1,7 @@
 import os
 import json
 import time
-import Apify
+import Apify # type: ignore
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
